6f35e724e5320350082981c50281b284146ac762
MarianoGalvan <galvan.mariano.r@gmail.com>
2022-07-07 18:59:43 -0300

    primer ejercicio de for

--- clase01/js/main.js
+++ clase01/js/main.js
@@ -1,11 +1,36 @@
+//este es el ejercicio de la primer entrega
+const listadoNotas = [5, 7, 3];
+
+/* const posicion = 1;
+
+listadoNotas[2];    //3
+listadoNotas[0];    //5
+listadoNotas[posicion]; //7 */
+
+let sumadorDeNotas = 0;
+
+for (let i = 0; i < 3; i++) {
+    sumadorDeNotas = sumadorDeNotas + listadoNotas[i];
+    // sumadorDeNotas += listadoNotas[i]; es lo mismo que lo de arriba
+}
+const promedio = sumadorDeNotas / 3;
+console.log(promedio);
+
+if (promedio >= 7) {
+    console.log("Aprobaste y aprobaste con un:" + promedio);
+} else {
+    console.log("no aprobaste");
+}
+//hasta acá llegó el ejercicio
+
 
 
 const listadoDeProductos = ['zapatillas', 'remeras', 'chanclas'];
 
 let acumuladorDeTexto = "";
 
-for(let i= 0; i < 3; i++) {
-    acumuladorDeTexto += "<div>" + listadoDeProductos[i]+"</div>";
+for (let i = 0; i < 3; i++) {
+    acumuladorDeTexto += "<div>" + listadoDeProductos[i] + "</div>";
 }
 
 console.log(acumuladorDeTexto);
@@ -16,19 +41,19 @@ let totalCarrito = 0;
 function agregarAlCarrito(producto, stock) {
     const tenemosStock = validarStock(stock);
 
-    if(tenemosStock === "Tenemos stock"){
-        console.log("Agregas el producto al carrito: "  + producto);
+    if (tenemosStock === "Tenemos stock") {
+        console.log("Agregas el producto al carrito: " + producto);
         totalCarrito++;
-    }else{
+    } else {
         console.log("Lo siento subió mucho el dolar");
-    }    
-    
+    }
+
 }
 
-function validarStock(stockDelProducto){
-    if (stockDelProducto > 0){
+function validarStock(stockDelProducto) {
+    if (stockDelProducto > 0) {
         return "tenemos stock";
-    } else{
+    } else {
         return "no tenemos stock";
     }
 }
