240653daeb80f8f6065ed60971324d3d5420df7f
MarianoGalvan <galvan.mariano.r@gmail.com>
2022-09-22 16:56:46 -0300

    main mas limpio

--- clase01/js/main.js
+++ clase01/js/main.js
@@ -45,38 +45,3 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 });
 
-let notas1 = parseInt(prompt("Ingrese la primer nota del alumno"))
-let notas2 = parseInt(prompt("Ingrese la segunda nota del alumno"))
-let notas3 = parseInt(prompt("Ingrese la tercera nota del alumno"))
-
-let listadoNotas1 = [notas1, notas2, notas3];
-
-function calcNotas(listadoNotas) {
-    let sumadorDeNotas = 0;
-    for (let i = 0; i < 3; i++) {
-        sumadorDeNotas = sumadorDeNotas + listadoNotas[i];
-        // sumadorDeNotas += listadoNotas[i]; es lo mismo que lo de arriba
-    }
-    const promedio = sumadorDeNotas / 3;
-    console.log(promedio);
-
-    if (promedio >= 7) {
-        console.log("Aprobaste y aprobaste con un:" + promedio);
-    } else {
-        console.log("no aprobaste");
-    }
-}
-
-calcNotas(listadoNotas1)
-
-let palabrasList = [];
-function palabras() {
-
-    for (let i = 0; i <= 2; i++) {
-        let palabra = prompt("Ingresa tu palabra: ");
-        palabrasList.push(palabra);
-    }
-}
-
-palabras();
-console.log(palabrasList);
